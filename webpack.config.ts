99f511d2a3551174d8f41a9fc2f9777940c155f4
Pig Fang <g-plane@hotmail.com>
2021-10-25 23:30:48 +0800

    Parcel 2 -> webpack

---
+++ webpack.config.ts
@@ -0,0 +1,75 @@
+import * as path from 'node:path'
+import type { Configuration } from 'webpack'
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+import merge from 'webpack-merge'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+
+export default (_: Record<string, string>, argv: Record<string, string>) => {
+  const isDev = argv.mode === 'development'
+
+  const baseConfig: Configuration & { devServer?: DevServerConfiguration } = {
+    entry: './src/main.tsx',
+    output: {
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'typescript',
+                tsx: true,
+                dynamicImport: true,
+              },
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                  refresh: isDev,
+                },
+              },
+              target: 'es2019',
+              loose: true,
+            },
+          },
+        },
+        {
+          test: /\.tc$/,
+          type: 'asset/source',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'index.html'),
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.tsx', '.ts'],
+    },
+    cache: {
+      type: 'filesystem',
+    },
+    devServer: {
+      hot: true,
+    },
+    devtool: 'eval-cheap-module-source-map',
+    stats: 'errors-warnings',
+    ignoreWarnings: [/size limit/i],
+  }
+
+  if (isDev) {
+    return merge(baseConfig, {
+      plugins: [new ReactRefreshWebpackPlugin()],
+    })
+  }
+
+  return merge(baseConfig, {
+    output: {
+      filename: '[name].[contenthash:8].js',
+    },
+  })
+}
