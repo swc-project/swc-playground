e175af822ad6b71eca1af63b88da74ee4954ad8b
Pig Fang <g-plane@hotmail.com>
2021-10-25 09:34:57 +0800

    add external links at header bar

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,4 +1,5 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, HStack, Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const logoURL = new URL('../../assets/swc.svg', import.meta.url).toString()
 
@@ -9,13 +10,20 @@ export default function HeaderBar() {
       justifyContent="space-between"
       h="56px"
       px="5"
-      py="1.5"
+      py="2"
       bg="gray.100"
       borderBottomWidth="1px"
       borderBottomColor="gray.300"
     >
       <img src={logoURL} alt="swc" />
-      <Flex></Flex>
+      <HStack spacing="4">
+        <Link href="https://swc.rs/" isExternal>
+          Docs <ExternalLinkIcon mb="1" />
+        </Link>
+        <Link href="https://github.com/swc-project/swc" isExternal>
+          GitHub <ExternalLinkIcon mb="1" />
+        </Link>
+      </HStack>
     </Flex>
   )
 }
