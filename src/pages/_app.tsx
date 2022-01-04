efecbc6e1103e072dfd13ac3c90dfd3961276170
Tim Neutkens <tim@timneutkens.nl>
2022-01-04 10:11:27 +0100

    Apply suggestions from code review

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
@@ -8,4 +8,4 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export default MyApp
+export default App
