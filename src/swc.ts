9dfc3e4c22c8e9c38b42ccfe6bfab61f76d1d03c
Pig Fang <g-plane@hotmail.com>
2022-01-21 09:37:25 +0800

    upgrade SWC (fix #13)

--- src/swc.ts
+++ src/swc.ts
@@ -255,7 +255,7 @@ export interface TransformationOutput {
 }
 
 export const swcVersionAtom = atom(
-  new URLSearchParams(location.search).get('version') ?? '1.2.124'
+  new URLSearchParams(location.search).get('version') ?? '1.2.133'
 )
 
 export async function loadSwc(version: string): Promise<SwcModule> {
